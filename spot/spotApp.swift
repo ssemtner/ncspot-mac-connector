8943ac828825b6fb0d6cfe2468f549c19ffd3a23
Scott Semtner <sjsemtner@gmail.com>
2025-06-23 10:38:32 -0700

    Initial Commit

---
+++ spot/spotApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotApp.swift
+//  spot
+//
+//  Created by Scott Semtner on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct spotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ spotTests/spotTests.swift
@@ -0,0 +1,17 @@
+//
+//  spotTests.swift
+//  spotTests
+//
+//  Created by Scott Semtner on 6/23/25.
+//
+
+import Testing
+@testable import spot
+
+struct spotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
